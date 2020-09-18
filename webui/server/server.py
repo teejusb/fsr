3094fef433ca71e44bcd4eef9c0a90778003cbce
teejusb <5017202+teejusb@users.noreply.github.com>
2020-09-18 03:15:04 -0700

    Start adding some actual serial code, also add plot things, and some other big revamps.

--- webui/server/server.py
+++ webui/server/server.py
@@ -1,40 +1,100 @@
-from random import randint
-from threading import Thread, Event
-import time
 import logging
+import queue
+import serial
+import time
+
 from flask import Flask
 from flask_socketio import SocketIO, emit
+from random import normalvariate, randint
+from threading import Thread, Event
 
 app = Flask(__name__)
 socketio = SocketIO(app, cors_allowed_origins="*")
 
 logging.getLogger('werkzeug').setLevel(logging.ERROR)
+logger = logging.getLogger(__name__)
 
-#random number Generator Thread
-thread = Thread()
+# Threads for the serial reader and writer.
+read_thread = Thread()
+write_thread = Thread()
 thread_stop_event = Event()
 
 cur_thresholds = [0, 0, 0, 0]
+# L, D, U, R
+sensor_numbers = [3, 2, 0, 1]
 
-def randomNumberGenerator():
-  """
-  Generate a random number every 1 second and emit to a socketio instance (broadcast)
-  Ideally to be run in a separate thread?
-  """
-  #infinite loop of magical random numbers
-  print("Making random numbers")
-  numbers = [randint(0, 1023) for _ in range(4)]
-  while not thread_stop_event.isSet():
-    offsets = [randint(-20, 20) for _ in range(4)]
-    numbers = [max(0, min(numbers[i] + offsets[i], 1023)) for i in range(4)]
-    print(str(numbers))
-    for i in range(4):
-      socketio.emit('newnumber' + str(i), {'value': numbers[i]})
-    socketio.sleep(0.05)
+class SerialHandler(object):
+  def __init__(self, port="", timeout=1):
+    self.ser = None
+    self.port = port
+    self.timeout = timeout
+    self.write_queue = queue.Queue(10)
+
+  def ChangePort(self, port):
+    if self.ser:
+      self.ser.close()
+      self.ser = None
+    self.port = port
+    self.Open()
+
+  def Open(self):
+    if not self.port:
+      return
+
+    if self.ser:
+      self.ser.close()
+      self.ser = None
+
+    try:
+      self.ser = serial.Serial(self.port, 9600, timeout=self.timeout)
+    except Exception as e:
+      self.ser = None
+      logger.exception("Error opening serial: %s", e)
+
+  def Read(self):
+    print("Making random numbers")
+    numbers = [randint(0, 1023) for _ in range(4)]
+    while not thread_stop_event.isSet():
+      offsets = [int(normalvariate(0, 5)) for _ in range(4)]
+      numbers = [max(0, min(numbers[i] + offsets[i], 1023)) for i in range(4)]
+      # print(str(numbers))
+      for i in range(4):
+        socketio.emit('newnumber' + str(i), {'value': numbers[i]})
+      socketio.sleep(0.01)
+      # if not self.ser:
+      #   self.Open()
+      # try:
+      #   # This will block the thread until it gets a newline
+      #   line = self.ser.readline()
+      #   values = line.split()[1:]
+      #   # We're printing Up, Right, Down, Left
+      #   if len(values) == 4:
+      #     for i, value in enumerate(values) :
+      #       socketio.emit('newnumber' + sensor_numbers[i], {'value': value})
+      # except serial.SerialException as e:
+      #   logger.error("Error reading data: ", e)
+      #   self.Open()
+
+  def Write(self):
+    while not thread_stop_event.isSet():
+      # if not self.ser:
+      #   # Just wait until the reader opens the serial port.
+      #   time.sleep(1)
+      #   continue
+
+      index, value = self.write_queue.get()
+      # self.ser.write(str(sensor_numbers[index]) + str(value) + "\n")
+
+      global cur_thresholds
+      cur_thresholds[index] = value
+      print('Thresholds are: ' + str(cur_thresholds))
+
+
+serial_handler = SerialHandler(port="")#/dev/tty/ACM0")
 
 @app.route('/defaults')
 def get_defaults():
-  return { 'thresholds': cur_thresholds }
+  return {}
 
 @app.route('/time')
 def get_current_time():
@@ -43,23 +103,30 @@ def get_current_time():
 
 @socketio.on('connect')
 def connect():
-  global thread
+  global read_thread
+  global write_thread
   print('Client connected')
-  if not thread.isAlive():
-    print("Starting Thread")
-    thread = socketio.start_background_task(randomNumberGenerator)
+  # TODO(teejusb): Fetch cur_thresholds from somewhere.
+  socketio.emit('thresholds', {'thresholds': cur_thresholds})
 
+  if not read_thread.isAlive():
+    print("Starting Read Thread")
+    read_thread = socketio.start_background_task(serial_handler.Read)
+
+  if not write_thread.isAlive():
+    print("Starting Write Thread")
+    write_thread = socketio.start_background_task(serial_handler.Write)
 
 @socketio.on('disconnect')
 def disconnect():
   print('Client disconnected')
 
-
 @socketio.on('update_threshold')
-def update_threshold(index, value):
-  global cur_thresholds
-  cur_thresholds[index] = value
-  print('Values are: ' + str(cur_thresholds))
+def update_threshold(values, index):
+  try:
+    serial_handler.write_queue.put((index, values[index]), block=False)
+  except queue.Full as e:
+    logger.error("Could not update thresholds. Queue full.")
 
 
 if __name__ == "__main__":
--- webui/src/App.js
+++ webui/src/App.js
@@ -22,7 +22,11 @@ import {
 } from "react-router-dom";
 
 let kCurThresholds = [0, 0, 0, 0];
-const socket = io.connect();
+const socket = io("127.0.0.1:5000");
+
+socket.on('thresholds', function(msg) {
+  kCurThresholds = msg.thresholds;
+});
 
 function NavBar() {
   return (
@@ -48,98 +52,134 @@ function NavBar() {
   );
 }
 
-function Slider(props) {
-  const [sliderVal, setSliderVal] = useState(kCurThresholds[parseInt(props.index)]);
+function Canvas(props) {
+  const index = parseInt(props.index)
+  const thresholdLabelRef = React.useRef(null);
+  const valueLabelRef = React.useRef(null);
+  const canvasRef = React.useRef(null);
 
-  function EmitSliderVal(val) {
-    socket.emit('update_threshold', parseInt(props.index), val);
+  function EmitValue(val) {
+    // Send back all the thresholds, in case the server restarts it would be
+    // nicer to have all the values in sync.
+    // Still send back the index since we want to update only one value at a time
+    // to the microcontroller.
+    socket.emit('update_threshold', kCurThresholds, index);
   }
 
   function Decrement(e) {
-    const val = sliderVal - 1;
+    const val = kCurThresholds[index] - 1;
     if (val >= 0) {
-      setSliderVal(val);
-      EmitSliderVal(val);
+      kCurThresholds[index] = val;
+      EmitValue(val);
     }
   }
   function Increment(e) {
-    const val = sliderVal + 1;
+    const val = kCurThresholds[index] + 1;
     if (val <= 1023) {
-      setSliderVal(val);
-      EmitSliderVal(val);
+      kCurThresholds[index] = val
+      EmitValue(val);
     }
   }
-  return (
-    <Row>
-      <Col xs="auto">
-        <Form.Label>{sliderVal.toString().padStart(4, "0")}</Form.Label>
-      </Col>
-      <Col xs="auto">
-        <Button variant="light" onClick={Decrement}><b>-</b></Button>
-      </Col>
-      <Col>
-        <Form.Control
-          type="range"
-          min="0"
-          max="1023"
-          width="100px"
-          value={sliderVal}
-          custom
-          onChange={e => setSliderVal(parseInt(e.target.value))}
-          onMouseUp={e => EmitSliderVal(parseInt(e.target.value))}
-        />
-      </Col>
-      <Col xs="auto">
-        <Button variant="light" onClick={Increment}><b>+</b></Button>
-      </Col>
-    </Row>
-  );
-}
-
-function Canvas(props) {
-  const [currentValue, setCurrentValue] = useState(0);
-  const labelRef = React.useRef(null);
-  const canvasRef = React.useRef(null);
 
   useEffect(() => {
     let requestId;
+    let currentValue = 0;
 
-    socket.on('newnumber' + props.index, function(msg) {
-      setCurrentValue(msg.value);
+    socket.on('newnumber' + index, function(msg) {
+      currentValue = msg.value;
       // console.log("Received numbers: " + kCurValues.toString());
     });
 
-    // ********** Canvas setup **********
-    const label = labelRef.current;
     const canvas = canvasRef.current;
 
-    // Adjust DPI so that all the edges are smooth during scaling.
-    const dpi = window.devicePixelRatio;
-    const style = {
-      height() {
-        return +getComputedStyle(canvas).getPropertyValue('height').slice(0,-2);
-      },
-      width() {
-        return +getComputedStyle(canvas).getPropertyValue('width').slice(0,-2);
-      }
+    // Change thresholds on drag.
+    function getMousePos(canvas, e) {
+      const rect = canvas.getBoundingClientRect();
+      return {
+        x: e.clientX - rect.left,
+        y: e.clientY - rect.top
+      };
     }
-    canvas.setAttribute('width', style.width() * dpi);
-    canvas.setAttribute('height', style.height() * dpi);
+    let is_drag = false;
 
-    // Add background fill.
-    const ctx = canvas.getContext('2d');
-    let grd = ctx.createLinearGradient(canvas.width/2, 0, canvas.width/2 ,canvas.height);
-    grd.addColorStop(0, 'lightblue');
-    grd.addColorStop(1, 'gray');
-    ctx.fillStyle = grd;
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    // Mouse Events
+    canvas.addEventListener('mousedown', function(e) {
+      let pos = getMousePos(canvas, e);
+      kCurThresholds[index] = Math.floor(1023 - pos.y/canvas.height * 1023);
+      is_drag = true;
+    });
 
-    // ***********************************
+    canvas.addEventListener('mouseup', function(e) {
+      let pos = getMousePos(canvas, e);
+      kCurThresholds[index] = Math.floor(1023 - pos.y/canvas.height * 1023);
+      EmitValue(kCurThresholds[index]);
+      is_drag = false;
+    });
+
+    canvas.addEventListener('mousemove', function(e) {
+      if (is_drag) {
+        let pos = getMousePos(canvas, e);
+        kCurThresholds[index] = Math.floor(1023 - pos.y/canvas.height * 1023);
+      }
+    });
+
+    // Touch Events
+    canvas.addEventListener('touchstart', function(e) {
+      let pos = getMousePos(canvas, e);
+      kCurThresholds[index] = Math.floor(1023 - pos.y/canvas.height * 1023);
+      is_drag = true;
+    });
+
+    canvas.addEventListener('touchend', function(e) {
+      let pos = getMousePos(canvas, e);
+      kCurThresholds[index] = Math.floor(1023 - pos.y/canvas.height * 1023);
+      EmitValue(kCurThresholds[index]);
+      is_drag = false;
+    });
+
+    canvas.addEventListener('touchmove', function(e) {
+      if (is_drag) {
+        let pos = getMousePos(canvas, e);
+        kCurThresholds[index] = Math.floor(1023 - pos.y/canvas.height * 1023);
+      }
+    });
 
     const render = () => {
+      // ********** Canvas setup **********
+      // This has to be here in case the user resizes the window.
+      // Adjust DPI so that all the edges are smooth during scaling.
+      const dpi = window.devicePixelRatio;
+      const style = {
+        height() {
+          return +getComputedStyle(canvas).getPropertyValue('height').slice(0,-2);
+        },
+        width() {
+          return +getComputedStyle(canvas).getPropertyValue('width').slice(0,-2);
+        }
+      }
+      canvas.setAttribute('width', style.width() * dpi);
+      canvas.setAttribute('height', style.height() * dpi);
+      // **********************************
+
+      // Add background fill.
+      const ctx = canvas.getContext('2d');
+      let grd = ctx.createLinearGradient(canvas.width/2, 0, canvas.width/2 ,canvas.height);
+      if (currentValue >= kCurThresholds[index]) {
+        grd.addColorStop(0, 'lightblue');
+        grd.addColorStop(1, 'blue');
+      } else {
+        grd.addColorStop(0, 'lightblue');
+        grd.addColorStop(1, 'gray');
+      }
+      ctx.fillStyle = grd;
+      ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+      // Cur Value Label
+      const valueLabel = valueLabelRef.current;
+      valueLabel.innerHTML = currentValue;
+
       // Bar
       const maxHeight = canvas.height;
-      label.innerHTML = currentValue;
       const position = maxHeight - currentValue/1023 * maxHeight;
       grd = ctx.createLinearGradient(canvas.width/2, canvas.height, canvas.width/2, position);
       grd.addColorStop(0, 'orange');
@@ -147,20 +187,44 @@ function Canvas(props) {
       ctx.fillStyle = grd;
       ctx.fillRect(canvas.width/4, position, canvas.width/2, canvas.height);
 
+      // Threshold
+      const threshold_height = 3
+      const threshold_pos = (1023-kCurThresholds[index])/1023 * canvas.height;
+      ctx.fillStyle = "black";
+      ctx.fillRect(0, threshold_pos-Math.floor(threshold_height/2),  canvas.width, threshold_height);
+
+      // Threshold Label
+      const thresholdLabel = thresholdLabelRef.current;
+      thresholdLabel.innerHTML = kCurThresholds[index];
+      ctx.font = "30px Arial";
+      ctx.fillStyle = "black";
+      if (kCurThresholds[index] > 990) {
+        ctx.textBaseline = 'top';
+      } else {
+        ctx.textBaseline = 'bottom';
+      }
+      ctx.fillText(kCurThresholds[index].toString(), 0, threshold_pos + threshold_height + 1);
+
       requestId = requestAnimationFrame(render);
     };
 
     render();
 
     return () => {
-      socket.off('newnumber' + props.index);
+      socket.off('newnumber' + index);
       cancelAnimationFrame(requestId);
     };
-  });
+  }, [index]);
 
   return(
-    <Col style={{height: '75vh'}}>
-      <Form.Label ref={labelRef}>0</Form.Label>
+    <Col style={{height: '75vh', paddingTop: '1vh'}}>
+      <Button variant="light" size="md" onClick={Decrement}><b>-</b></Button>
+      <span> </span>
+      <Button variant="light" size="md" onClick={Increment}><b>+</b></Button>
+      <br />
+      <Form.Label ref={thresholdLabelRef}>0</Form.Label>
+      <br />
+      <Form.Label ref={valueLabelRef}>0</Form.Label>
       <canvas
         ref={canvasRef}
         style={{border: '1px solid white', width: '100%', height: '100%'}}
@@ -173,10 +237,6 @@ function WebUI() {
   return (
     <header className="App-header">
       <Container fluid style={{border: '1px solid white', height: '100vh'}}>
-        <Slider index="0"/>
-        <Slider index="1"/>
-        <Slider index="2"/>
-        <Slider index="3"/>
         <Row>
           <Canvas index="0"/>
           <Canvas index="1"/>
@@ -188,6 +248,112 @@ function WebUI() {
   );
 }
 
+function Plot() {
+  const canvasRef = React.useRef(null);
+
+  useEffect(() => {
+    let requestId;
+    const canvas = canvasRef.current;
+
+    let currentValues = []
+    const max_size = 1000;
+    let oldest = 0;
+
+    socket.on('newnumber0', function(msg) {
+      if (currentValues.length < max_size) {
+        currentValues.push(msg.value);
+      } else {
+        currentValues[oldest] = msg.value;
+        oldest = (oldest + 1) % max_size;
+      }
+    });
+
+    const render = () => {
+      // ********** Canvas setup **********
+      // This has to be here in case the user resizes the window.
+      // Adjust DPI so that all the edges are smooth during scaling.
+      const dpi = window.devicePixelRatio;
+      const style = {
+        height() {
+          return +getComputedStyle(canvas).getPropertyValue('height').slice(0,-2);
+        },
+        width() {
+          return +getComputedStyle(canvas).getPropertyValue('width').slice(0,-2);
+        }
+      }
+      canvas.setAttribute('width', style.width() * dpi);
+      canvas.setAttribute('height', style.height() * dpi);
+      // **********************************
+
+      // Add background fill.
+      const ctx = canvas.getContext('2d');
+      let grd = ctx.createLinearGradient(canvas.width/2, 0, canvas.width/2 ,canvas.height);
+      grd.addColorStop(0, 'white');
+      grd.addColorStop(1, 'lightgray');
+      ctx.fillStyle = grd;
+      ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+      const spacing = 10;
+      const box_width = canvas.width-spacing*2;
+      const box_height = canvas.height-spacing*2
+      ctx.beginPath();
+      ctx.rect(spacing, spacing, box_width, box_height);
+      ctx.stroke();
+
+      function drawDashedLine(ctx, pattern, spacing, y, width) {
+        ctx.beginPath();
+        ctx.setLineDash(pattern);
+        ctx.moveTo(spacing, y);
+        ctx.lineTo(width, y);
+        ctx.stroke();
+      }
+
+      const num_divisions = 5;
+      for (let i = 1; i < num_divisions; ++i) {
+        drawDashedLine(ctx, [20, 5], spacing, box_height-(box_height/num_divisions)*i + spacing, box_width + spacing);
+      }
+
+      const px_per_div = box_width/max_size;
+      ctx.beginPath();
+      ctx.setLineDash([]);
+      for (let i = 0; i < max_size; ++i) {
+        if (i === currentValues.length) { break; }
+        if (i === 0) {
+          ctx.moveTo(px_per_div*i + spacing, box_height - box_height * currentValues[(i + oldest) % max_size]/1023 + spacing);
+        } else {
+          ctx.lineTo(px_per_div*i + spacing, box_height - box_height * currentValues[(i + oldest) % max_size]/1023 + spacing);
+        }
+      }
+      ctx.stroke();
+
+      ctx.font = "30px Arial";
+      ctx.fillStyle = "black";
+      if (currentValues.length < max_size) {
+        ctx.fillText(currentValues[currentValues.length-1], 100, 100);
+      } else {
+        ctx.fillText(currentValues[((oldest - 1) % max_size + max_size) % max_size], 100, 100);
+      }
+
+      requestId = requestAnimationFrame(render);
+    };
+
+    render();
+
+    return () => {
+      socket.off('newnumber0');
+      cancelAnimationFrame(requestId);
+    };
+  }, []);
+  return (
+    <header className="App-header">
+      <canvas
+        ref={canvasRef}
+        style={{border: '1px solid white', width: '100%', height: '100%'}}
+      />
+    </header>
+  );
+}
+
 function App() {
   const [currentTime, setCurrentTime] = useState(0);
   const [fetched, setFetched] = useState(false);
@@ -195,7 +361,6 @@ function App() {
   useEffect(() => {
     fetch('/defaults').then(res => res.json()).then(data => {
       if (!fetched) {
-        kCurThresholds = data.thresholds;
         setFetched(true);
       }
     });
@@ -223,9 +388,7 @@ function App() {
               </header>
             </Route>
             <Route path="/plot">
-              <header className="App-header">
-                <p>Plot</p>
-              </header>
+              <Plot />
             </Route>
           </Switch>
         </Router>
