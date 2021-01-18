3fa2470ab91534de10c0c5f56e594a7bece4c17f
teejusb <5017202+teejusb@users.noreply.github.com>
2021-01-18 12:46:40 -0800

    Change the default threshold to 1000. Seems like triggering the panels before the UI has loaded in causes issues for some.

--- fsr.ino
+++ fsr.ino
@@ -30,7 +30,7 @@
 #endif
 
 // Default threshold value for each of the sensors.
-const int16_t kDefaultThreshold = 200;
+const int16_t kDefaultThreshold = 1000;
 // Max window size for both of the moving averages classes.
 const size_t kWindowSize = 50;
 // Baud rate used for Serial communication. Technically ignored by Teensys.
